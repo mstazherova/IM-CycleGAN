4f820af724b0eae0f0c001cb75234aed89457878
mstazherova <stazherova@gmail.com>
2018-09-18 08:53:48 +0200

    changed training loop

--- build.py
+++ build.py
@@ -1,28 +1,26 @@
 import os
 import argparse
 import time 
-import numpy as np
 
 import tensorflow as tf
 
-from input import A, B
 from model import *
+from utils import *
+from images import Images
+from image_cache import ImageCache
 
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+# os.environ["CUDA_VISIBLE_DEVICES"]="0"
+# os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 
-config = tf.ConfigProto(log_device_placement=True)
+# config = tf.ConfigProto(log_device_placement=True)
 # config.gpu_options.per_process_gpu_memory_fraction = 0.5
 # config.gpu_options.allow_growth = True
-sess = tf.Session(config=config)
+# sess = tf.Session(config=config)
 
-# ensure reproducability
 np.random.seed(1234)
 
 start = time.time()
 
-CHECKPOINT_FILE = 'cyclegan.ckpt'
-CHECKPOINT_DIR = './checkpoint/'
 LOG_DIR = './log/'
 
 WIDTH = 256
@@ -30,40 +28,32 @@ HEIGHT = 256
 CHANNEL = 3
 BATCH_SIZE = 1
 
+SAMPLE_STEP = 10
+SAVE_STEP = 500
 
-def save_model(saver, sess, counter):
-    if not os.path.isdir(CHECKPOINT_DIR):
-        os.makedirs(CHECKPOINT_DIR)
-    path = os.path.join(CHECKPOINT_DIR, CHECKPOINT_FILE)
-    saver.save(sess, path, global_step=counter)
-    return path
+    
+def build_model(input_a, input_b, gen_a_sample, gen_b_sample):
+    with tf.variable_scope('model', reuse=tf.AUTO_REUSE):
+        g1 = generator(input_a, name='g_a2b')     # input A -> generated sample B 
+        g2 = generator(input_b, name='g_b2a')     # input B -> generated sample A
+        d_a = discriminator(input_a, name='d_a')  # input A -> [0, 1]
+        d_b = discriminator(input_b, name='d_b')  # input B -> [0, 1]
 
+        d_gen_b = discriminator(g1, name='d_b')   # generated sample B -> [0, 1]
+        d_gen_a = discriminator(g2, name='d_a')   # generated sample A -> [0, 1]
+        cycle_a = generator(g1, name='g_b2a')     # generated B -> reconstructed A
+        cycle_b = generator(g2, name='g_a2b')     # generated A -> reconstructed B
 
-def build_model(input_a, input_b):
-    #TODO create a class (maybe)
+        d_a_sample = discriminator(gen_a_sample, name='d_a')
+        d_b_sample = discriminator(gen_b_sample, name='d_b')
 
-    with tf.variable_scope('model') as scope:
-        #TODO rename a and b into source and target domains
-        g1 = generator(input_a, 'g_a2b')     # input A -> generated sample B 
-        g2 = generator(input_b, 'g_b2a')     # input B -> generated sample A
-        d_a = discriminator(input_a, 'd_a')  # input A -> [0, 1]
-        d_b = discriminator(input_b, 'd_b')  # input B -> [0, 1]
+       
+    # Discriminator loss 
+    d_a_loss_real = tf.reduce_mean(d_a - tf.ones_like(d_a))
+    d_a_loss_fake = tf.reduce_mean(d_a_sample - tf.zeros_like(d_a_sample))
 
-        scope.reuse_variables()
-
-        d_gen_b = discriminator(g1, 'd_b')  # generated sample B -> [0, 1]
-        d_gen_a = discriminator(g2, 'd_a')  # generated sample A -> [0, 1]
-        cycle_a = generator(g1, 'g_b2a')    # generated B -> reconstructed A
-        cycle_b = generator(g2, 'g_a2b')    # generated A -> reconstructed B
-
-    # Discriminator loss
-    # recommendation for images from A must be close to 1
-    d_a_loss_real = tf.reduce_mean(tf.squared_difference(d_a, 1))  
-    d_b_loss_real = tf.reduce_mean(tf.squared_difference(d_b, 1))
-
-    # predicting 0 for images produced by the generator
-    d_a_loss_fake = tf.reduce_mean(tf.square(d_gen_a))  
-    d_b_loss_fake = tf.reduce_mean(tf.square(d_gen_b))
+    d_b_loss_real = tf.reduce_mean(d_b - tf.ones_like(d_b))
+    d_b_loss_fake = tf.reduce_mean(d_b_sample - tf.zeros_like(d_b_sample))
 
     d_a_loss = tf.reduce_mean(d_a_loss_real + d_a_loss_fake)
     d_b_loss = tf.reduce_mean(d_b_loss_real + d_b_loss_fake)
@@ -95,68 +85,68 @@ def build_model(input_a, input_b):
     g_b_train_op = tf.train.AdamOptimizer(2e-4).minimize(g_total_b, 
                                                          var_list=g_b_vars)
 
-    print('The model is built! It took {} seconds'.format(time.time() - start))
+    print('Built the model, took {0:.2f} seconds'.format(time.time() - start))
 
-    return d_a_loss, d_b_loss, g_total_a, g_total_b, d_a_train_op, \
-           d_b_train_op, g_a_train_op, g_b_train_op, g1, g2
+    return d_a_train_op, d_b_train_op, g_a_train_op, g_b_train_op, g1, g2
     
 
-def main(epochs, num_img):
+def main(epochs):
     """Main loop."""
 
     tf.reset_default_graph() 
 
-    A_input, B_input = A, B
+    DATA_PATH = 'data/horse2zebra/'
+    input_a = Images(DATA_PATH + '_trainA.tfrecords', batch_size=BATCH_SIZE, 
+                     name='A_input').feed()
+    input_b = Images(DATA_PATH + '_trainB.tfrecords', batch_size=BATCH_SIZE, 
+                     name='B_input').feed()
+    test_A = Images(DATA_PATH + '_testA.tfrecords', name='test_a').feed()
+    test_B = Images(DATA_PATH + '_testB.tfrecords', name='test_b').feed()
+    
+    gen_a_sample = tf.placeholder(tf.float32, [None, 256, 256, 3], name="fake_a_sample")
+    gen_b_sample = tf.placeholder(tf.float32, [None, 256, 256, 3], name="fake_b_sample")
 
-    input_a = tf.placeholder(dtype=tf.float32, 
-                             shape=[None, HEIGHT, WIDTH, CHANNEL],
-                             name='input_a')
-    input_b = tf.placeholder(dtype=tf.float32, 
-                             shape=[None, HEIGHT, WIDTH, CHANNEL], 
-                             name='input_b')
+    d_a_train_op, d_b_train_op, g_a_train_op, g_b_train_op, g1, g2 = build_model(input_a, input_b, gen_a_sample, gen_b_sample)
 
-    d_a_loss, d_b_loss, g_a_loss, g_b_loss, d_a_train_op, d_b_train_op, \
-    g_a_train_op, g_b_train_op, g1, g2 = build_model(input_a, input_b)
+    # testG1 = generator(test_A, name='g_a2b')
+    # testG2 = generator(test_B,  name='g_b2a')
+    # testCycleA = generator(testG1,  name='d_a')
+    # testCycleB = generator(testG2, name='d_b')
+    
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
 
-    with sess:
-        sess.run(tf.global_variables_initializer())
+    with tf.Session() as sess:
+        sess.run(init)
+        ckpt = tf.train.get_checkpoint_state('./checkpoint/')
+
+        cache_a = ImageCache(50)
+        cache_b = ImageCache(50)
+
+        counter = 0
         for epoch in range(epochs):
-            for step in range(num_img):  # batch size = 1
-                a_input = A_input[step].reshape(1, HEIGHT, WIDTH, CHANNEL)
-                b_input = B_input[step].reshape(1, HEIGHT, WIDTH, CHANNEL)
+            print('Beginning epoch {}...'.format(epoch+1))
 
-                # G a -> b, D_b
-                _, gen_b_tmp = sess.run([g_a_train_op, g1], 
-                                        feed_dict={input_a:a_input, 
-                                                   input_b:b_input})
-                _ = sess.run([d_b_train_op], 
-                             feed_dict={input_a:a_input, 
-                                        input_b:b_input})
-                # G b -> a, D_a
-                _, gen_a_tmp = sess.run([g_b_train_op, g2], 
-                                        feed_dict={input_a:a_input, 
-                                                   input_b:b_input})
-                _ = sess.run([d_a_train_op], 
-                             feed_dict={input_a:a_input, 
-                                        input_b:b_input})
-            d_loss_a = sess.run(d_a_loss, {input_a: a_input, input_b: b_input})
-            g_loss_a = g_a_loss.eval({input_a: a_input, input_b: b_input})
-            d_loss_b = sess.run(d_b_loss, {input_a: a_input, input_b: b_input})
-            g_loss_b = g_b_loss.eval({input_a: a_input, input_b: b_input})
-            print("Epoch {}/{}...".format(epoch+1, epochs))
-            print("Discriminator A Loss: {:.4f}...".format(d_loss_a))
-            print("Generator A Loss: {:.4f}".format(g_loss_a))
-            print("Discriminator B Loss: {:.4f}...".format(d_loss_b))
-            print("Generator B Loss: {:.4f}".format(g_loss_b))
+            _, gen_b = sess.run([g_a_train_op, g1])
+            print('Gen_b done')
+            _, gen_a = sess.run([g_b_train_op, g2]) 
+
+            _ = sess.run([d_b_train_op, d_a_train_op], 
+                         feed_dict={gen_b_sample: cache_b.fetch(gen_b),
+                                    gen_a_sample: cache_a.fetch(gen_a)})
+
+            counter += 1
+            print('{:4d} time: {:4.4f}'.format(counter, time.time() - start))
+                
+            if np.mod(counter, SAVE_STEP) == 0:
+                save_path = save_model(saver, sess, counter)
+                print('Running for {0:.2} mins, saving to {}'.format((time.time() - start) / 60, save_path))
             
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-e', '--epochs', type=int, default=50, 
                         help='Number of epochs. Default:50')
-    parser.add_argument('-i', '--images', type=int, default=1000,
-                        help='Number of training images/batches. Default:1000')
     args = parser.parse_args()
 
-    main(args.epochs, args.images)
+    main(args.epochs)
--- model.py
+++ model.py
@@ -1,29 +1,27 @@
 import tensorflow as tf
 
 
-def lrelu(x, leak=0.2, name="lrelu"):
+def lrelu(x, leak=0.2, name='lrelu'):
     return tf.maximum(x, leak * x)
 
 
-def batch_norm(x, name='batch_norm'):
-    with tf.variable_scope(name):
-        return tf.layers.batch_normalization(x, epsilon=1e-5)
+def batch_norm(x):
+    return tf.layers.batch_normalization(x, epsilon=1e-5)
 
 
-def conv2d(inputs, out_dim, fs=4, s=2, padding='same', name='conv2d'):
-    with tf.variable_scope(name):
-        return tf.layers.conv2d(inputs=inputs, filters=out_dim, 
-                                kernel_size=fs, strides=(s,s), padding=padding)
+def conv2d(inputs, out_dim, fs=4, s=2, padding='same'):
+    return tf.layers.conv2d(inputs=inputs, filters=out_dim, 
+                            kernel_size=fs, strides=(s,s), padding=padding,
+                            kernel_initializer=tf.truncated_normal_initializer())
 
 
-def deconv2d(inputs, out_dim, fs=4, s=2, padding='same', name='deconv2d'):
-    with tf.variable_scope(name):
-        return tf.layers.conv2d_transpose(inputs=inputs, filters=out_dim, 
-                                          kernel_size=fs, strides=(s,s),
-                                          padding=padding)
+def deconv2d(inputs, out_dim, fs=4, s=2, padding='same'):
+    return tf.layers.conv2d_transpose(inputs=inputs, filters=out_dim, 
+                                      kernel_size=fs, strides=(s,s),
+                                      padding=padding)
 
 
-def resnet_block(x, out_dim, fs=3, s=1, p=1, mode='REFLECT', name='res'):
+def resnet_block(x, out, fs=3, s=1, p=1, mode='REFLECT', name='res'):
     """A layer that consists of 2 convolutional layers where a residue of input 
     is added to the output. CycleGAN code has two differences from typical 
     residual blocks:
@@ -32,9 +30,9 @@ def resnet_block(x, out_dim, fs=3, s=1, p=1, mode='REFLECT', name='res'):
     # TODO add instance norm
     paddings = [[0, 0], [p, p], [p, p], [0, 0]]
     y = tf.pad(x, paddings, mode=mode)
-    y = conv2d(y, out_dim, fs, s, padding='valid', name=name + '_c1')
+    y = conv2d(y, out, fs, s, padding='valid')
     y = tf.pad(tf.nn.relu(y), paddings, mode=mode)
-    y = conv2d(y, out_dim, fs, s, padding='valid', name=name + '_c2')
+    y = conv2d(y, out, fs, s, padding='valid')
 
     return y + x
 
@@ -42,38 +40,39 @@ def resnet_block(x, out_dim, fs=3, s=1, p=1, mode='REFLECT', name='res'):
 def generator(x, name='generator'):
     """Builds the generator that consists of an encoder, 
     a transformer and a decoder."""
-    with tf.variable_scope(name):
-        with tf.variable_scope('encoder'):
-            #TODO change activation functions, add batch norm
-            enc = tf.pad(x, [[0, 0], [3, 3], [3, 3], [0, 0]], "REFLECT")
-            enc1 = tf.nn.relu(conv2d(enc, 32, 7, 1, padding='valid'))
-            enc2 = tf.nn.relu(conv2d(enc1, 64, 3, 2))
-            enc_out = tf.nn.relu(conv2d(enc2, 128, 3, 2))
-        with tf.variable_scope('transformer'):
-            res1 = resnet_block(enc_out, 128)
-            res2 = resnet_block(res1, 128)
-            res3 = resnet_block(res2, 128)
-            res4 = resnet_block(res3, 128)
-            res5 = resnet_block(res4, 128)
-            res_out = resnet_block(res5, 128)
-        with tf.variable_scope('decoder'):
-            dec1 = tf.nn.relu(deconv2d(res_out, 64, 3, 2))
-            dec2 = tf.nn.relu(deconv2d(dec1, 32, 3, 2))
-            dec2 = tf.pad(dec2, [[0, 0], [3, 3], [3, 3], [0, 0]], "REFLECT")
-            gen_out = tf.nn.tanh(conv2d(dec2, 3, 7, 1, padding='valid'))
-    
-    return gen_out
+    with tf.variable_scope(name, reuse=tf.AUTO_REUSE):
+        # TODO add batch norm
+        # Encoder 
+        enc = tf.pad(x, [[0, 0], [3, 3], [3, 3], [0, 0]], "REFLECT")
+        enc1 = tf.nn.relu(conv2d(enc, 32, 7, 1, padding='valid'))
+        enc2 = tf.nn.relu(conv2d(enc1, 64, 3, 2))
+        enc_out = tf.nn.relu(conv2d(enc2, 128, 3, 2))
+        # Transformer 
+        res1 = resnet_block(enc_out, 128)
+        res2 = resnet_block(res1, 128)
+        res3 = resnet_block(res2, 128)
+        res4 = resnet_block(res3, 128)
+        res5 = resnet_block(res4, 128)
+        res_out = resnet_block(res5, 128)  
+        # Decoder
+        dec1 = tf.nn.relu(deconv2d(res_out, 64, 3, 2))
+        dec2 = tf.nn.relu(deconv2d(dec1, 32, 3, 2))
+        dec2_pad = tf.pad(dec2, [[0, 0], [3, 3], [3, 3], [0, 0]], "REFLECT")
+        dec3 = conv2d(dec2_pad, 3, 7, 1, padding='valid')
+        dec_out = tf.nn.tanh(dec3)
+
+        return dec_out
 
 
 def discriminator(x, name='discriminator'):
     """Builds the discriminator which is a fully-convolutional 
     network."""
-    with tf.variable_scope(name):
+    with tf.variable_scope(name, reuse=tf.AUTO_REUSE):
         #TODO add batch normalization
-        h1 = lrelu(conv2d(x, 64, [4, 4], [2, 2]))
-        h2 = lrelu(tf.layers.conv2d(h1, 128, [4, 4], [2, 2]))
-        h3 = lrelu(tf.layers.conv2d(h2, 256, [4, 4], [2, 2]))
-        h4 = lrelu(tf.layers.conv2d(h3, 512, [4, 4], [2, 2]))
-        out = tf.layers.conv2d(h4, 1, [4, 4], [1, 1])
+        h1 = lrelu(conv2d(x, 64))
+        h2 = lrelu(conv2d(h1, 128))
+        h3 = lrelu(conv2d(h2, 256))
+        h4 = lrelu(conv2d(h3, 512, s=1))
+        out = conv2d(h4, 1, s=1)
 
-    return out
+        return out
