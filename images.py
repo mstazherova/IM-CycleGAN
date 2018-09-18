d7029b4d278887b88950a4003a5afa505596c3f4
mstazherova <stazherova@gmail.com>
2018-09-18 21:57:47 +0200

    minor fix

--- images.py
+++ images.py
@@ -8,37 +8,17 @@ class Images():
         self.name = name
 
 
-    # def feed(self):
-    #     with tf.name_scope(self.name):
-    #         filename_queue = tf.train.string_input_producer([self.tfrecords])
-    #         reader = tf.TFRecordReader()
-
-    #         _, serialized = reader.read(filename_queue)
-    #         features = tf.parse_single_example(
-    #             serialized, features={
-    #                 'image/file_name': tf.FixedLenFeature([], tf.string),
-    #                 'image/encoded_image': tf.FixedLenFeature([], tf.string)})
-            
-    #         image_buffer = features['image/encoded_image']
-    #         image = tf.image.decode_jpeg(image_buffer, channels=3)
-    #         image = self.preprocess(image)
-    #         images = tf.train.batch([image], batch_size=self.batch_size) 
-    #         # tf.summary.image('_input', images)
-    #     return images
-
-
     def extract_fn(self, data_record):
         features = {'image/file_name': tf.FixedLenFeature([], tf.string),
                     'image/encoded_image': tf.FixedLenFeature([], tf.string)}
         sample = tf.parse_single_example(data_record, features)
         image = tf.image.decode_jpeg(sample['image/encoded_image'], channels=3)        
-        # filename = sample['image/file_name']
         image = self.preprocess(image)
+        
         return image 
 
 
-    # TODO rename this function
-    def feed_test(self):
+    def feed(self):
         dataset = tf.data.TFRecordDataset(self.tfrecords)
         dataset = dataset.map(self.extract_fn)
         dataset = dataset.batch(self.batch_size)
@@ -48,7 +28,6 @@ class Images():
         return next_image_data
 
      
-
     def preprocess(self, image):
         image = tf.image.resize_images(image, 
                                        size=(self.image_size, self.image_size))
