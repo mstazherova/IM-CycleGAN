051fc00ed93c218ed50a6c169d0361fa0c064bb2
mstazherova <stazherova@gmail.com>
2018-11-09 14:43:06 +0100

    changed summaries, changed discriminator loss

--- build.py
+++ build.py
@@ -42,16 +42,23 @@ def build_model(input_a, input_b, gen_a_sample, gen_b_sample):
         d_b_sample = discriminator(gen_b_sample, name='d_b')
        
     # Discriminator loss 
-    d_a_loss_real = tf.reduce_mean((d_a - tf.ones_like(d_a) * np.abs(np.random.normal(1.0, SOFT))) ** 2)
-    d_a_loss_fake = tf.reduce_mean((d_a_sample - tf.zeros_like(d_a_sample)) ** 2)
+    # d_a_loss_real = tf.reduce_mean((d_a - tf.ones_like(d_a) * np.abs(np.random.normal(1.0, SOFT))) ** 2)
+    # d_a_loss_fake = tf.reduce_mean((d_a_sample - tf.zeros_like(d_a_sample)) ** 2)
 
-    d_b_loss_real = tf.reduce_mean((d_b - tf.ones_like(d_b) * np.abs(np.random.normal(1.0, SOFT))) ** 2)
-    d_b_loss_fake = tf.reduce_mean((d_b_sample - tf.zeros_like(d_b_sample)) ** 2)
+    # d_b_loss_real = tf.reduce_mean((d_b - tf.ones_like(d_b) * np.abs(np.random.normal(1.0, SOFT))) ** 2)
+    # d_b_loss_fake = tf.reduce_mean((d_b_sample - tf.zeros_like(d_b_sample)) ** 2)
+    # mean squared error
+    d_a_loss_real = tf.reduce_mean(tf.squared_difference(d_a, 0.9))
+    d_a_loss_fake = tf.reduce_mean(tf.square(d_a_sample))
+
+    d_b_loss_real = tf.reduce_mean(tf.squared_difference(d_b, 0.9))
+    d_b_loss_fake = tf.reduce_mean(tf.square(d_b_sample))
+
 
     d_a_loss = (d_a_loss_real + d_a_loss_fake) / 2
     d_b_loss = (d_b_loss_real + d_b_loss_fake) / 2
-    tf.summary.scalar('D_a Loss', d_a_loss)
-    tf.summary.scalar('D_b loss', d_b_loss)
+    d_a_sum = tf.summary.scalar('D_a Loss', d_a_loss)
+    d_b_sum = tf.summary.scalar('D_b loss', d_b_loss)
 
     # Generator loss
     g_a_loss = tf.reduce_mean((d_gen_b - tf.ones_like(d_gen_b) * np.abs(np.random.normal(1.0, SOFT))) ** 2)
@@ -63,8 +70,8 @@ def build_model(input_a, input_b, gen_a_sample, gen_b_sample):
 
     g_total_a = g_a_loss + 10 * cycle_loss
     g_total_b = g_b_loss + 10 * cycle_loss
-    tf.summary.scalar('G_a Loss', g_total_a)
-    tf.summary.scalar('G_b Loss', g_total_b)
+    g_a_sum = tf.summary.scalar('G_a Loss', g_total_a)
+    g_b_sum = tf.summary.scalar('G_b Loss', g_total_b)
 
     # Optimizers
     trainable_vars = tf.trainable_variables()
@@ -82,20 +89,22 @@ def build_model(input_a, input_b, gen_a_sample, gen_b_sample):
     g_b_train_op = tf.train.AdamOptimizer(2e-4).minimize(g_total_b, 
                                                          var_list=g_b_vars)
 
-    tf.summary.image('Input A', input_a, max_outputs=5)
-    tf.summary.image('Generated B', g1, max_outputs=5)
-    tf.summary.image('Input B', input_b, max_outputs=5)
-    tf.summary.image('Generated A', g2, max_outputs=5)
+    a_sum = tf.summary.image('Input A', input_a, max_outputs=1)
+    g1_sum = tf.summary.image('Generated B', g1, max_outputs=1)
+    b_sum = tf.summary.image('Input B', input_b, max_outputs=1)
+    g2_sum = tf.summary.image('Generated A', g2, max_outputs=1)
+
+    d_sum = tf.summary.merge([d_a_sum, d_b_sum])
+    g_sum = tf.summary.merge([g_a_sum, g_b_sum, a_sum, g1_sum, b_sum, g2_sum])
     
     print('Built the model in {0:.2f} seconds'.format(time.time() - start))
 
     return d_a_loss, d_b_loss, g_total_a, g_total_b, d_a_train_op, \
-           d_b_train_op, g_a_train_op, g_b_train_op, g1, g2
+           d_b_train_op, g_a_train_op, g_b_train_op, g1, g2, d_sum, g_sum
     
 
 def main(arguments):
     """Main loop."""
-    # TODO rename constants
     EPOCHS = arguments.epochs
     GPU = arguments.gpu
     GPU_NUMBER = arguments.gpu_number
@@ -127,13 +136,13 @@ def main(arguments):
     gen_b_sample = tf.placeholder(tf.float32, [None, WIDTH, HEIGHT, CHANNEL], name="fake_b_sample")
 
     d_a_loss, d_b_loss, g_a_loss, g_b_loss, d_a_train_op, d_b_train_op, \
-    g_a_train_op, g_b_train_op, g1, g2 = build_model(train_A, train_B, gen_a_sample, gen_b_sample)
+    g_a_train_op, g_b_train_op, g1, g2, d_sum, g_sum = build_model(train_A, train_B, gen_a_sample, gen_b_sample)
 
     testG1 = generator(test_A, name='g_a2b')
     testG2 = generator(test_B,  name='g_b2a')
     testCycleA = generator(testG1,  name='d_a')
     testCycleB = generator(testG2, name='d_b')
-    merged = tf.summary.merge_all()
+    # merged = tf.summary.merge_all()
     
     init = tf.global_variables_initializer()
     saver = tf.train.Saver()
@@ -147,19 +156,22 @@ def main(arguments):
 
         print('Beginning training...')
         for epoch in range(EPOCHS):
-            start = time.time()
+            start = time.perf_counter()
             sess.run(it_a)
             sess.run(it_b)
             try:
-                for step in range(1067):  # TODO change number of steps
-                    gen_a, gen_b = sess.run([g1, g2])
-                    _, _, _, _, summaries = sess.run([g_a_train_op, d_b_train_op, 
-                                                      g_b_train_op, d_a_train_op, merged],
-                                                     feed_dict={gen_b_sample: cache_b.fetch(gen_b),
-                                                                gen_a_sample: cache_a.fetch(gen_a)})
+                for step in tqdm(range(1067)):  # TODO change number of steps
+                    gen_a, gen_b, _, _, summaries = sess.run([g1, g2, g_a_train_op, 
+                                                              g_b_train_op, g_sum])
+                    _, _, summaries = sess.run([d_b_train_op, d_a_train_op, d_sum],
+                                               feed_dict={gen_b_sample: cache_b.fetch(gen_b),
+                                                          gen_a_sample: cache_a.fetch(gen_a)})
                     if step % 100 == 0:
                         writer.add_summary(summaries, epoch * 1067 + step)
-            except tf.errors.OutOfRangeError:
+
+            except tf.errors.OutOfRangeError as e:
+                print(e)
+                print("Out of range: {}".format(step))
                 pass  
            
             print("Epoch {}/{} done.".format(epoch+1, EPOCHS))
@@ -168,7 +180,7 @@ def main(arguments):
                 
             if np.mod(counter, SAVE_STEP) == 0:
                 save_path = save_model(saver, sess, counter)
-                print('Running for {:.2f} mins, saving to {}'.format((time.time() - start) / 60, save_path))
+                print('Running for {:.2f} mins, saving to {}'.format((time.perf_counter() - start) / 60, save_path))
 
             if TO_SAMPLE == 1 and np.mod(counter, SAMPLE_STEP) == 0:
                 sample(it_at, it_bt, sess, counter, test_A, test_B, testG1, testG2, testCycleA, testCycleB)
